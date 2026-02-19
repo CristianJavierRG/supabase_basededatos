9573d0c87bb1064859eafe09f173491f763fbf66
Cristian Javier Rubio Gayosso <cristianjaviergayosso@gmail.com>
2026-02-19 13:47:51 +0000

    SCHEMA del proyecto

--- agenda_mvc/mvc/controllers/index_controller.py
+++
@@ -1,9 +0,0 @@
-import web
-from models.persona_model import PersonaModel
-
-render = web.template.render("views/")
-
-class IndexController:
-    def GET(self):
-        result = PersonaModel.obtener_todos()
-        return render.index({"result": result.data})
